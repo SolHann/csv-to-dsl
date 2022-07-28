70f369a6f73067785bfe1b24e1aad56a2fced674
SolHann <supersolhann@gmail.com>
2022-07-28 13:53:28 +0100

    Change the format of the csv that is accepted

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import csv
+import sys
 # -----------------------------------------------------------
 # converts a csv file into a dsl
 #
-# The format of the csv must be in format NAME,DESCRIPTION,RELATIONSHIP NAME, RELATIONSHIP to, DESCRIPTION, TAG(colour)
+# The format of the csv must be:
+# Name,Description,Tag,RelationshipName,RelationShipTo,Description,RelationshipTag,
 #
-# email:
-# github:
+# Only Name and RelationshipTo are required. Any other column can be left blank.
+#
+# Run in command line with a csv file as the argument. Example:
+# Python3 main.py example_csv
+#
 # -----------------------------------------------------------
 
-# Change these constants for different default themes
+# Change these constants for different default style.
 SUPPLIER_FONTSIZE = "50"
 RELATIONSHIP_FONTSIZE = "42"
 RELATIONSHIP_COLOUR = "#707070"
 
-file_name = input("CSV FILE NAME (include .csv): ")
-csv_file = open(file_name, newline='')
+file = sys.argv[1]
+csv_file = open(file, newline='')
 reader = csv.reader(csv_file)
 
 prev_name = ""
 name = ""
+
 with open('dsl_code.txt', 'w') as f:
     f.write("workspace {\n"
             "   model {"
@@ -28,10 +34,12 @@ with open('dsl_code.txt', 'w') as f:
     for row in reader:
         name = row[0]
         if prev_name == name:
-            continue # this stops the creation of duplicate persons
+            continue    # If there are consecutive duplicate names don't create new entities
         f.write("       " +
-                row[0] +" = person \""+ row[0]
-                + "\" \"" + row[1] + "\"\n")
+                row[0] + " = person \"" + row[0]
+                + "\" \"" + row[1]  # description
+                + "\" \"" + row[2] + "\"\n"  # tag
+                )
         prev_name = row[0]
 
     f.write("       enterprise = enterprise \"Diagram\"{ \n "
@@ -40,15 +48,16 @@ with open('dsl_code.txt', 'w') as f:
     # This makes the reader go back to the start of the csv
     csv_file.seek(0)
     reader.__next__()
+
     for row in reader:
-         f.write("               " +
-                row[0] +" -> "+ row[3]
-                + " \"" + row[2] + "\" \""
-                + row[4] + "  "
-                + row[5] + "\" \""
-                + row[6] + "\" \n")
+        f.write("               " +
+                row[0] + " -> " + row[4]
+                + " \"" + row[3] + "\" \""  # Relationship name
+                + row[5] + "\" \""  # Description
+                + row[6] + "\" \n")  # Tag
     f.write("           }\n      }\n   }\n"
-            "   views {\n       systemLandscape {\n           include * \n       }\n"
+            "   views {\n       systemLandscape "
+            "{\n           include * \n       }\n"
             "       styles { \n"
             "         element Person { \n"
             "             shape person \n"
@@ -57,7 +66,7 @@ with open('dsl_code.txt', 'w') as f:
             "       }\n"
             "          relationship Relationship { \n"
             "             fontSize " + RELATIONSHIP_FONTSIZE + "\n"
-            "             colour " + RELATIONSHIP_COLOUR +  "\n"                                                           
+            "             colour " + RELATIONSHIP_COLOUR + "\n"                                                           
             "       }\n     }\n     theme default\n   }\n}"
             ""
             )
